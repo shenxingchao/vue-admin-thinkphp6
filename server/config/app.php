8cc36cb2f35cfd8485fb7f63fce852123386a85e
shenxingchao <864318466@qq.com>
2020-12-30 22:28:25 +0800

    1.修改虚拟映射域名

--- server/config/app.php
+++ server/config/app.php
@@ -21,7 +21,7 @@ return [
     'app_map' => [],
     // 域名绑定（自动多应用模式有效）
     'domain_bind' => [
-        'www.vue-element-thinkphp6.com' => 'admin',
+        'www.vue-admin-thinkphp6.com' => 'admin',
     ],
     // 禁止URL访问的应用列表（自动多应用模式有效）
     'deny_app_list' => [],
