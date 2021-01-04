5766e7abeb2a23d2db517073868bc40c44494d7c
shenxingchao <864318466@qq.com>
2021-01-04 09:01:05 +0800

    1.路由分组

--- server/app/admin/route/app.php
+++ server/app/admin/route/app.php
@@ -2,5 +2,13 @@
 use think\facade\Route;
 
 Route::get('hello', 'Test/hello');
-//路由资源添加
-Route::post('RouteResource/routeResourceAdd', 'RouteResource/routeResourceAdd');
+
+//不需要鉴权的常规路由
+Route::group('normal', function () {
+});
+
+//需要鉴权的路由分组
+Route::group('auth', function () {
+    //路由资源添加
+    Route::post('RouteResource/routeResourceAdd', 'RouteResource/routeResourceAdd');
+});
