749c521166bad4431c86451671e64e3d062c2bbc
shenxingchao <864318466@qq.com>
2021-01-06 00:47:21 +0800

    1.定制apidoc模板

--- server/apidoc-template/utils/handlebars_helper.js
+++ server/apidoc-template/utils/handlebars_helper.js
@@ -143,6 +143,13 @@ define([
     /**
      *
      */
+    Handlebars.registerHelper('toUpperCase', function(value) {
+        return (value && typeof value === 'string') ? value.toUpperCase() : '';
+    });
+
+    /**
+     *
+     */
     Handlebars.registerHelper('splitFill', function(value, splitChar, fillChar) {
         var splits = value.split(splitChar);
         return new Array(splits.length).join(fillChar) + splits[splits.length - 1];
